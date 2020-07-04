8ce66bd0fa6f6fc6765e1aca1f63b8827e982434
Abhishek <sawai.abhishek@gmail.com>
2020-07-04 19:51:44 +0530

    added author 751

--- JenkinGiT/src/regressionPack/TC_VerifyLogin.java
+++ JenkinGiT/src/regressionPack/TC_VerifyLogin.java
@@ -18,6 +18,7 @@ public class TC_VerifyLogin {
 		System.out.println("added 729");
 		System.out.println("added 741");
 		System.out.println("added 742");
+		System.out.println("added 750 added my name to author");
 		
 		
 		String titlefrombrd = "Facebook – log in or sign up";
