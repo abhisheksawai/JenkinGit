92168dfc727b477710865982b8ced0229f35e272
Abhishek <sawai.abhishek@gmail.com>
2020-07-04 19:22:46 +0530

    first Commit 722

---
+++ JenkinGiT/src/regressionPack/TC_VerifyLogin.java
@@ -0,0 +1,36 @@
+package regressionPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_VerifyLogin {
+	
+	@Test
+	public void logincheck()
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenovo\\Desktop\\Current Batch\\Batch Data\\jar\\chrome 84\\chromedriver.exe");
+		//ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();		
+		driver.get("https://www.facebook.com/");
+		
+		String titlefrombrd = "Facebook – log in or sign up";
+		String gettitleFromPortal = driver.getTitle();
+		System.out.println("actual title " + gettitleFromPortal);
+
+		if(titlefrombrd.equals(gettitleFromPortal))
+		{
+			System.out.println("tc pass");
+			System.out.println("actula is "+titlefrombrd+ " but found on portal"+gettitleFromPortal);
+		}
+		else
+		{
+			System.out.println("tc failed");
+			System.out.println("actula is "+titlefrombrd+ " but found on portal "+gettitleFromPortal);
+		}
+		
+	
+	}
+
+}
